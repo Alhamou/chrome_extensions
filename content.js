1e71b9bf7b44433fb1e809c93c414d701be5684a
Emad Alhamou <emadalhamo@gmail.com>
2019-04-21 18:51:28 +0200

    add chrome.browserAction.setBadgeText({'text': num.toString() });

--- content.js
+++ content.js
@@ -1,36 +1,47 @@
 (function(){
 
 
-    $('#send').on('click', function (e){
+    $('#send').on('click', function (){
 
         var text = $('#type_name').val();
-        console.log(text);
-        chrome.storage.sync.get('list', function (items){
-            
-            let data = [...items.list || [] ,...[{ "title": text }]];
-            
-            chrome.storage.sync.set({ list:data});
+        if (text){
 
-            $('ul').append(`
-            <li>${text}</li>
-            `);
-
-            $('#type_name').val('');
-        });
+            chrome.storage.sync.get('list', function (items){
+                
+                let data = [...items.list || [] ,...[{ "title":  text}]];
+                
+                chrome.storage.sync.set({ list:data});
+    
+                $('ul').append(`<li>${text}</li>`);
+    
+                $('#type_name').val('');
+            });
+        }
     
     });
     chrome.storage.sync.get('list', function (items){
-        items.list.forEach(element => {
-            $('ul').append(`
-            <li>${element.title}</li>
-            `);
-        });
+        try{
+            items.list.forEach(element => {
+                $('ul').append(`
+                <li>${element.title}</li>
+                `);
+            });
+        } catch {}
         
     });
 
     $('#delete').on('click', function (){
         $('ul').empty();
         chrome.storage.sync.remove('list');
+
+        var opt = {
+            type: "basic",
+            title: "Actension Title",
+            message: "Primary message to display",
+            iconUrl: "icon.png"
+          }
+          chrome.notifications.create("delete",opt);
+
     });
 
 })()
---
+++ eventPage.js
@@ -0,0 +1,8 @@
+chrome.storage.onChanged.addListener(function(changes){
+    try{
+        var num = changes.list.newValue.length;
+    }catch {
+        var num = 0;
+    }
+    chrome.browserAction.setBadgeText({'text': num.toString() });
+});
