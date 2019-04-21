e3b8e1c5b860eea06ed0fa6df2ddfa7ec44c5c5a
Emad Alhamou <emadalhamo@gmail.com>
2019-04-21 16:14:34 +0200

    add storage to array

--- content.js
+++ content.js
@@ -1,5 +1,21 @@
 (function(){
-    $('#type_name').on('keyup', function (e){
-        console.log( e.target.value )
+
+
+    $('#send').on('click', function (e){
+
+        var text = $('#type_name').val();
+        console.log(text);
+        chrome.storage.sync.get('list', function (items){
+            
+            let data = [...items.list || [] ,...[{ "title": text }]];
+            
+            chrome.storage.sync.set({ list:data});
+
+        });
+    
     });
+    chrome.storage.sync.get('list', function (items){
+        console.table(items);
+    });
+
 })()
